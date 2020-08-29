b7a8538f69ed37bc6a795e352604a2480a2c192b
Balázs <hello@sarkozibalazs.dev>
2020-08-30 00:39:46 +0200

    Added the projects page

---
+++ pages/_project.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="project">
+        <div v-for="(val, locale) in project.fields.title">
+            <h1 v-if="locale === 'en-US'">
+                {{val}}
+            </h1>
+        </div>
+
+        <div v-for="(val, locale) in project.fields.content">
+            <div v-if="locale === 'en-US'" v-html="$md.render(val)">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {contentfulClient} from '../services/contentful'
+
+    export default {
+        data(){
+            return {
+                project: null
+            }
+        },
+        async fetch() {
+            const client = await contentfulClient();
+
+            const project = await client.getEntries({
+                content_type: "blogPost",
+                'fields.slug.en-US' : this.$route.params.project
+            })
+
+
+            this.project = project.items[0]
+        }
+    }
+</script>
+
+<style lang="scss">
+    h1{
+        color: #000;
+        font-size: 3em;
+        font-weight:400;
+    }
+
+    .project{
+        font-size: 20px;
+    }
+</style>
