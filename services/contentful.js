cb777f5e8498652d1c67f10513223e4a7564c312
Balázs <hello@sarkozibalazs.dev>
2020-08-30 00:40:14 +0200

    Deployment update, removed env vars

--- services/contentful.js
+++ services/contentful.js
@@ -1,9 +1,5 @@
 import * as contentful from "contentful-management"
 
-//TODO: remove
-process.env.token = "CFPAT-dv8nKVy8Tp2MTC0Am4-qSd-hIIqT8xyBiTsAIQXddx4";
-process.env.space = "af3cho9ux0wm";
-
 export async function contentfulClient(){
     const client = contentful.createClient({
         accessToken: process.env.token
